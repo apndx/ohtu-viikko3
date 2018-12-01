fee74807f52d76a255a9553fb8dae34193f3c9dd
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2018-12-01 09:52:49 +0200

    viikko6: 1. Laskin ja komento-oliot

---
+++ ohtu-viikko6/Laskin/src/main/java/laskin/Komento.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laskin;
+
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author apndx
+ */
+public interface Komento {
+    void suorita(int arvo);
+    void peru();
+    TextField getSyotekentta();
+    
+}
---
+++ ohtu-viikko6/Laskin/src/main/java/laskin/Nollaa.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author apndx
+ */
+public class Nollaa implements Komento {
+
+    private final TextField tuloskentta;
+    private final TextField syotekentta;
+    private final Button nollaa;
+    private final Button undo;
+    private final Sovelluslogiikka sovellus;
+
+    public Nollaa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        this.tuloskentta = tuloskentta;
+        this.syotekentta = syotekentta;
+        this.nollaa = nollaa;
+        this.undo = undo;
+        this.sovellus = sovellus;
+    }
+
+    @Override
+    public void suorita(int arvo) {
+        sovellus.nollaa();
+        
+        int laskunTulos = sovellus.tulos();
+
+        syotekentta.setText("");
+        tuloskentta.setText("" + laskunTulos);
+
+        if (laskunTulos == 0) {
+            nollaa.disableProperty().set(true);
+        } else {
+            nollaa.disableProperty().set(false);
+        }
+        undo.disableProperty().set(false);
+    }
+
+    @Override
+    public void peru() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public TextField getSyotekentta() {
+        return syotekentta;
+    }
+
+}
