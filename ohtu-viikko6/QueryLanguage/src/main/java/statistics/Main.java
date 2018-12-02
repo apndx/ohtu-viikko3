01891e0d7c533f317c1181506561b8f60ba9d17e
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2018-12-02 20:12:14 +0200

    viikko 6: 4. Parannettu kyselykieli, osa 1

--- ohtu-viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ ohtu-viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -1,6 +1,5 @@
 package statistics;
 
-import statistics.matcher.Not;
 import statistics.matcher.*;
 
 public class Main {
@@ -8,50 +7,76 @@ public class Main {
     public static void main(String[] args) {
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
 
-        System.out.println("And-lista");
-        Matcher m = new And(new HasAtLeast(10, "goals"),
-                new HasAtLeast(10, "assists"),
-                new PlaysIn("PHI")
-        );
+        Querybuilder query = new Querybuilder();
+//        Matcher all = query.build();
+//
+//        System.out.println("List of all players:");
+//        for (Player player : stats.matches(all)) {
+//            System.out.println(player);
+//        }
 
-        for (Player player : stats.matches(m)) {
+        Matcher team = query.playsIn("NYR").build();
+
+        System.out.println(" ");
+        System.out.println("NYR-players:");
+        for (Player player : stats.matches(team)) {
             System.out.println(player);
         }
 
         System.out.println(" ");
-        System.out.println("Not-lista");
-        Matcher not = new Not(new HasAtLeast(1, "goals"));
-        for (Player player : stats.matches(not)) {
+        System.out.println("Team NYR, at least 10 but less than 25 goals");
+
+        Matcher many = query.playsIn("NYR")
+                .hasAtLeast(10, "goals")
+                .hasFewerThan(25, "goals").build();
+
+        for (Player player : stats.matches(many)) {
             System.out.println(player);
         }
 
-        System.out.println(" ");
-        System.out.println("Or-lista");
-
-        Matcher or = new Or(new HasAtLeast(40, "goals"),
-                new HasAtLeast(60, "assists"),
-                new HasAtLeast(85, "points")
-        );
-
-        for (Player player : stats.matches(or)) {
-            System.out.println(player);
-        }
-
-        System.out.println(" ");
-        System.out.println("Fewer than -lista");
-
-        Matcher fewer = new HasFewerThan(1, "goals");
-
-        for (Player player : stats.matches(fewer)) {
-            System.out.println(player);
-        }
-
-        System.out.println(" ");
-        System.out.println("All-lista");
-        Matcher all = new All();
-        for (Player player : stats.matches(all)) {
-            System.out.println(player);
-        }
-
+//        System.out.println("And-lista");
+//        Matcher m = new And(new HasAtLeast(10, "goals"),
+//                new HasAtLeast(10, "assists"),
+//                new PlaysIn("PHI")
+//        );
+//
+//        for (Player player : stats.matches(m)) {
+//            System.out.println(player);
+//        }
+//
+//        System.out.println(" ");
+//        System.out.println("Not-lista");
+//        Matcher not = new Not(new HasAtLeast(1, "goals"));
+//        for (Player player : stats.matches(not)) {
+//            System.out.println(player);
+//        }
+//
+//        System.out.println(" ");
+//        System.out.println("Or-lista");
+//
+//        Matcher or = new Or(new HasAtLeast(40, "goals"),
+//                new HasAtLeast(60, "assists"),
+//                new HasAtLeast(85, "points")
+//        );
+//
+//        for (Player player : stats.matches(or)) {
+//            System.out.println(player);
+//        }
+//
+//        System.out.println(" ");
+//        System.out.println("Fewer than -lista");
+//
+//        Matcher fewer = new HasFewerThan(1, "goals");
+//
+//        for (Player player : stats.matches(fewer)) {
+//            System.out.println(player);
+//        }
+//
+//        System.out.println(" ");
+//        System.out.println("All-lista");
+//        Matcher all = new All();
+//        for (Player player : stats.matches(all)) {
+//            System.out.println(player);
+//        }
     }
 }
---
+++ ohtu-viikko6/QueryLanguage/src/main/java/statistics/matcher/Querybuilder.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistics.matcher;
+
+/**
+ *
+ * @author apndx
+ */
+public class Querybuilder {
+
+    Matcher matcher;
+
+    public Querybuilder() {
+        matcher = new And(new All());
+    }
+
+    public Matcher build() {
+        return matcher;
+    }
+
+    public Querybuilder playsIn(String team) {
+        this.matcher = new And(this.matcher, new PlaysIn(team));
+        return this;
+    }
+
+    public Querybuilder hasAtLeast(int value, String category) {
+        this.matcher = new And(this.matcher, new HasAtLeast(value, category));
+        return this;
+    }
+
+    public Querybuilder hasFewerThan(int value, String category) {
+        this.matcher = new And(this.matcher, new HasFewerThan(value, category));
+        return this;
+    }
+
+}
